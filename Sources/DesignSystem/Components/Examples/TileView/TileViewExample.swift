16c2ed6add786872b6e099fcbd20a4401f036142
Сергеев Эдуард <sergeeveo@akbarsdigital.ru>
2023-09-12 19:58:13 +0700

    PCABO3-8735 + пример использования TileView

---
+++ Sources/DesignSystem/Components/Examples/TileView/TileViewExample.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+private func example() {
+    let view = TileView()
+    var viewProperties = TileView.ViewProperties(
+        icon: .init(variant: .icon(UIImage())),
+        text: .init(string: "Example"),
+        action: { print("Example") })
+    view.create(with: viewProperties)
+    viewProperties = TileViewStyle.resize(size: .large, viewProperties: viewProperties)
+    viewProperties = TileViewStyle.restyle(style: .primary, viewProperties: viewProperties)
+    view.update(with: viewProperties)
+}
