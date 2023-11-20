37c8dc70ad1c835cca1c49a2149b2190af2b6d75
Сергеев Эдуард <sergeeveo@akbarsdigital.ru>
2023-11-20 13:55:10 +0700

    PCABO3-8069 + InputAmountView

---
+++ Sources/DesignSystem/Components/InputAmountView/InputAmountTextField.swift
@@ -0,0 +1,61 @@
+import UIKit
+
+public final class InputAmountTextField: UITextField {
+    
+    public struct ViewProperties {
+        public var text: NSMutableAttributedString
+        public var textAttributes: [NSAttributedString.Key: Any]
+        public var placeholder: NSMutableAttributedString
+        public var cursorColor: UIColor
+        public var delegateAssigningClosure: (UITextField) -> Void
+        
+        public init(
+            text: NSMutableAttributedString = "".attributed,
+            textAttributes: [NSAttributedString.Key: Any] = [:],
+            placeholder: NSMutableAttributedString = "".attributed,
+            cursorColor: UIColor = .contentPrimary,
+            delegateAssigningClosure: @escaping (UITextField) -> Void = { _ in }
+        ) {
+            self.text = text
+            self.textAttributes = textAttributes
+            self.placeholder = placeholder
+            self.cursorColor = cursorColor
+            self.delegateAssigningClosure = delegateAssigningClosure
+        }
+    }
+    
+    private var viewProperties: ViewProperties = .init()
+    
+    public func create(with viewProperties: ViewProperties) {
+        self.viewProperties = viewProperties
+        setupView()
+        updateView(viewProperties: viewProperties)
+    }
+    
+    public func update(with viewProperties: ViewProperties) {
+        updateView(viewProperties: viewProperties)
+        self.viewProperties = viewProperties
+    }
+    
+    private var wasSetup = false
+    
+    private func setupView() {
+        guard !wasSetup else { return }
+        wasSetup = true
+        keyboardType = .decimalPad
+        autocapitalizationType = .none
+        autocorrectionType = .no
+        spellCheckingType = .no
+    }
+    
+    private func updateView(viewProperties: ViewProperties) {
+        viewProperties.delegateAssigningClosure(self)
+        attributedText = viewProperties.text
+        defaultTextAttributes = viewProperties.textAttributes
+        typingAttributes = viewProperties.textAttributes
+        attributedPlaceholder = viewProperties.placeholder
+        tintColor = viewProperties.cursorColor
+    }
+    
+    public override var canBecomeFirstResponder: Bool { true }
+}
---
+++ Sources/DesignSystem/Components/InputAmountView/InputAmountView.swift
@@ -0,0 +1,129 @@
+import UIKit
+import SnapKit
+
+public final class InputAmountView: UIView {
+    
+    public struct ViewProperties {
+        public var title: NSMutableAttributedString
+        public var hint: NSMutableAttributedString
+        public var textFieldProperties: InputAmountTextField.ViewProperties
+        public var amountSymbol: NSMutableAttributedString
+        public var isUserInteractionEnabled: Bool
+        
+        public init(
+            title: NSMutableAttributedString = "".attributed,
+            hint: NSMutableAttributedString = "".attributed,
+            textFieldProperties: InputAmountTextField.ViewProperties = .init(),
+            amountSymbol: NSMutableAttributedString = "".attributed,
+            isUserInteractionEnabled: Bool = true
+        ) {
+            self.title = title
+            self.hint = hint
+            self.textFieldProperties = textFieldProperties
+            self.amountSymbol = amountSymbol
+            self.isUserInteractionEnabled = isUserInteractionEnabled
+        }
+    }
+    
+    private var viewProperties: ViewProperties = .init()
+    
+    private var titleLabel: UILabel = {
+        let label = UILabel()
+        label.numberOfLines = 0
+        return label
+    }()
+    private var amountTextField: InputAmountTextField = {
+        let textField = InputAmountTextField()
+        // Для кликабельности всей вьюхи
+        textField.isUserInteractionEnabled = false
+        return textField
+    }()
+    private var currencyLabel = UILabel()
+    private var hintLabel: UILabel = {
+        let label = UILabel()
+        label.numberOfLines = 0
+        label.snp.makeConstraints {
+            $0.height.greaterThanOrEqualTo(16)
+        }
+        return label
+    }()
+    
+    private lazy var vStack: UIStackView = {
+        let stack = UIStackView(arrangedSubviews: [
+            Spacer(size: 4),
+            titleLabel,
+            Spacer(size: 4),
+            amountCurrencyStack,
+            Spacer(size: 4),
+            hintLabel,
+            Spacer(size: 4)
+        ])
+        stack.axis = .vertical
+        stack.alignment = .leading
+        stack.spacing = .zero
+        return stack
+    }()
+    
+    private lazy var amountCurrencyStack: UIStackView = {
+        let stack = UIStackView(arrangedSubviews: [
+            amountTextField,
+            currencyLabel
+        ])
+        stack.axis = .horizontal
+        stack.alignment = .top
+        stack.spacing = 8
+        return stack
+    }()
+    
+    public func create(with viewProperties: ViewProperties) {
+        self.viewProperties = viewProperties
+        setupView()
+        amountTextField.create(with: viewProperties.textFieldProperties)
+        updateView(viewProperties: viewProperties)
+    }
+    
+    public func update(with viewProperties: ViewProperties) {
+        amountTextField.update(with: viewProperties.textFieldProperties)
+        updateView(viewProperties: viewProperties)
+        self.viewProperties = viewProperties
+    }
+    
+    private var wasSetup = false
+    
+    private func setupView() {
+        guard !wasSetup else { return }
+        wasSetup = true
+        addSubview(vStack)
+        vStack.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+    }
+    
+    private func updateView(viewProperties: ViewProperties) {
+        isUserInteractionEnabled = viewProperties.isUserInteractionEnabled
+        titleLabel.attributedText = viewProperties.title
+        currencyLabel.attributedText = viewProperties.amountSymbol
+        hintLabel.attributedText = viewProperties.hint
+        addGestureRecognizer(UITapGestureRecognizer(
+            target: self,
+            action: #selector(didTap)))
+    }
+    
+    @objc
+    private func didTap() {
+        guard isUserInteractionEnabled else { return }
+        amountTextField.becomeFirstResponder()
+    }
+}
+
+private class Spacer: UIView {
+    
+    init(
+        size: CGFloat = .zero
+    ) {
+        super.init(frame: .zero)
+        snp.makeConstraints { $0.size.equalTo(size) }
+    }
+    
+    @available(*, unavailable) required init?(coder aDecoder: NSCoder) { fatalError() }
+}
