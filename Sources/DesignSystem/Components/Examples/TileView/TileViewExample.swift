95fa2bc73bf91c90dfb4f3870f37ecb358ca2441
Сергеев Эдуард <sergeeveo@akbarsdigital.ru>
2024-03-18 15:07:42 +0300

    Приведение TileView к стандарту

--- Sources/DesignSystem/Components/Examples/TileView/TileViewExample.swift
+++ Sources/DesignSystem/Components/Examples/TileView/TileViewExample.swift
@@ -6,7 +6,6 @@ private func example() {
         icon: .init(variant: .icon(UIImage())),
         text: .init(string: "Example"),
         action: { print("Example") })
-    view.create(with: viewProperties)
     viewProperties = TileViewStyle.resize(size: .large, viewProperties: viewProperties)
     viewProperties = TileViewStyle.restyle(style: .primary, viewProperties: viewProperties)
     view.update(with: viewProperties)
