9017abc6417370e7f17e46f1f9aa0597eba94cf6
Сергеев Эдуард <sergeeveo@akbarsdigital.ru>
2024-04-01 16:39:54 +0700

    PCABO3-10005 + стили и примеры использования PaymentButton

---
+++ Sources/DesignSystem/Components/Examples/PaymentButtonExample.swift
@@ -0,0 +1,13 @@
+import UIKit
+import Components
+
+private func example() {
+    let view = PaymentButton()
+    let style = PaymentButtonStyle()
+    var viewProperties = PaymentButton.ViewProperties(
+        text: "Добавить в".attributed,
+        image: .ic24Bill.tinted(with: style.tintColor()),
+        onTap: { print("tapped") })
+    style.update(viewProperties: &viewProperties)
+    view.update(with: viewProperties)
+}
---
+++ Sources/DesignSystem/Components/Styles/PaymentButtonStyle.swift
@@ -0,0 +1,17 @@
+import UIKit
+import Components
+import Colors
+
+public struct PaymentButtonStyle {
+    
+    public func update(viewProperties: inout PaymentButton.ViewProperties) {
+        viewProperties.height = 56
+        viewProperties.backgroundColor = .backgroundMainInverse
+        viewProperties.cornerRadius = 8
+        viewProperties.text = viewProperties.text
+            .fontStyle(.textM)
+            .foregroundColor(tintColor())
+    }
+    
+    public func tintColor() -> UIColor { .contentActionOn }
+}
