5d3936cd85c0164c154c4315ae3a9f571d3ae2a0
Сергеев Эдуард <sergeeveo@akbarsdigital.ru>
2024-04-10 15:46:39 +0700

    PCABO3-10011 + пример с ручным вводом кода

--- Sources/DesignSystem/Components/Examples/InputOTPViewExample.swift
+++ Sources/DesignSystem/Components/Examples/InputOTPViewExample.swift
@@ -1,4 +1,4 @@
-import Foundation
+import UIKit
 import Components
 
 private func example() {
@@ -25,4 +25,89 @@ private func hintProperties() -> HintView.ViewProperties {
         variant: .right("Hint".attributed),
         viewProperties: &viewProperties)
     return viewProperties
+}
+
+private class InputOTPFeature: NSObject, UITextFieldDelegate {
+    
+    private var maxCount: Int = 5
+    
+    let view = InputOTPView()
+    private let itemStyle = InputOTPItemViewStyle()
+    private let hintStyle = HintViewStyle()
+    private lazy var viewProperties = InputOTPView.ViewProperties(
+        items: (0..<maxCount).map { _ in .init() },
+        hint: .init())
+    
+    private(set) lazy var textField: UITextField = {
+        let field = UITextField()
+        field.keyboardType = .numberPad
+        field.textContentType = .oneTimeCode
+        field.addTarget(self, action: #selector(onTextChange(textField:)), for: .editingChanged)
+        field.delegate = self
+        field.isHidden = true
+        return field
+    }()
+    
+    public override init() {
+        super.init()
+        setupView()
+    }
+    
+    private func setupView() {
+        view.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(fieldTapped)))
+        view.addSubview(textField)
+        reset()
+    }
+    
+    private func reset() {
+        for i in viewProperties.items.indices {
+            viewProperties.items[i].text = "".attributed
+            itemStyle.update(state: .default, viewProperties: &viewProperties.items[i])
+        }
+        hintStyle.update(variant: .empty, viewProperties: &viewProperties.hint)
+        view.update(with: viewProperties)
+    }
+    
+    @objc private func onTextChange(textField: UITextField) {
+        let text = (textField.text ?? "").map { "\($0)" }
+        for i in viewProperties.items.indices {
+            if i < text.count {
+                viewProperties.items[i].text = "\(text[i])".attributed
+                itemStyle.update(state: .active, viewProperties: &viewProperties.items[i])
+            } else {
+                viewProperties.items[i].text = "".attributed
+                itemStyle.update(state: .default, viewProperties: &viewProperties.items[i])
+            }
+        }
+        view.update(with: viewProperties)
+    }
+    
+    @objc private func fieldTapped() {
+        textField.becomeFirstResponder()
+    }
+    
+    func textField(
+        _ textField: UITextField,
+        shouldChangeCharactersIn range: NSRange,
+        replacementString string: String
+    ) -> Bool {
+        guard
+            string.isEmpty
+                || CharacterSet(charactersIn: string).isSubset(of: .decimalDigits),
+            let text = textField.text,
+            text.count + string.count <= maxCount
+        else { return false }
+        return true
+    }
+    
+    func showLeftError(message: String) {
+        textField.resignFirstResponder()
+        hintStyle.update(
+            variant: .left(message.attributed.foregroundColor(.contentError)),
+            viewProperties: &viewProperties.hint)
+        for i in viewProperties.items.indices {
+            itemStyle.update(state: .error, viewProperties: &viewProperties.items[i])
+        }
+        view.update(with: viewProperties)
+    }
 }
