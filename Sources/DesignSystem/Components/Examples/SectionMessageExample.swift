a949910a6525e4c152301083b2cff975e50390e9
Сергеев Эдуард <sergeeveo@akbarsdigital.ru>
2024-04-11 16:56:12 +0700

    PCABO3-10257 ~ приведение стилей SectionMessage к стандарту

--- Sources/DesignSystem/Components/Examples/SectionMessageExample.swift
+++ Sources/DesignSystem/Components/Examples/SectionMessageExample.swift
@@ -1,19 +1,71 @@
 import UIKit
+import SnapKit
 import Components
 
-private func example() {
-    let view = SectionMessageView()
+private class SectionMessageVC: UIViewController {
     
-    var viewProperties = SectionMessageView.ViewProperties(
-        title: .init(string: "Some text here"),
-        backgroundColor: .red
-    )
-    view.update(with: viewProperties)
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let vStack = vStack()
+        
+        vStack.addArrangedSubview({
+            let view = SectionMessageView()
+            let style = SectionMessageStyle(style: .info, size: .sizeS)
+            var viewProperties = SectionMessageView.ViewProperties(
+                title: "Label".attributed,
+                subtitle: "Content".attributed,
+                bottomButton: .init(
+                    text: "Label".attributed,
+                    action: { print("action") }))
+            style.update(with: &viewProperties)
+            view.update(with: viewProperties)
+            return view
+        }())
+        
+        vStack.addArrangedSubview({
+            let view = SectionMessageView()
+            let style = SectionMessageStyle(style: .warning, size: .sizeS)
+            var viewProperties = SectionMessageView.ViewProperties(
+                title: "Label".attributed)
+            style.update(with: &viewProperties)
+            view.update(with: viewProperties)
+            return view
+        }())
+        
+        vStack.addArrangedSubview({
+            let view = SectionMessageView()
+            let style = SectionMessageStyle(style: .success, size: .sizeS)
+            var viewProperties = SectionMessageView.ViewProperties(
+                subtitle: "Content".attributed)
+            style.update(with: &viewProperties)
+            view.update(with: viewProperties)
+            return view
+        }())
+        
+        vStack.addArrangedSubview({
+            let view = SectionMessageView()
+            let style = SectionMessageStyle(style: .error, size: .sizeS)
+            var viewProperties = SectionMessageView.ViewProperties(
+                bottomButton: .init(
+                    text: "Label".attributed,
+                    action: { print("action") }))
+            style.update(with: &viewProperties)
+            view.update(with: viewProperties)
+            return view
+        }())
+        
+        view.addSubview(vStack)
+        vStack.snp.makeConstraints {
+            $0.centerY.equalToSuperview()
+            $0.leading.trailing.equalToSuperview().inset(16)
+        }
+    }
     
-    SectionMessageStyle.error.update(with: &viewProperties)
-    view.update(with: viewProperties)
-    
-    SectionMessageStyle.success.update(with: &viewProperties)
-    view.update(with: viewProperties)
-    
+    private func vStack() -> UIStackView {
+        let stack = UIStackView()
+        stack.axis = .vertical
+        stack.spacing = 16
+        return stack
+    }
 }
--- Sources/DesignSystem/Components/Styles/SectionMessageStyle.swift
+++ Sources/DesignSystem/Components/Styles/SectionMessageStyle.swift
@@ -1,55 +1,133 @@
-//
-//  SectionMessageStyle.swift
-//  
-//
-//  Created by firdavs on 13.06.2023.
-//
-
-import ImagesService
 import UIKit
+import ImagesService
 import Components
 
-public enum SectionMessageStyle {
-    case info
-    case warning
-    case success
-    case error
-    case security
+public struct SectionMessageStyle {
+    
+    public enum Style {
+        case info
+        case warning
+        case success
+        case error
+        case neutral
+    }
+    
+    public enum Size {
+        case sizeS
+        case sizeM
+    }
+    
+    private let style: Style
+    private let size: Size
+    
+    public init(
+        style: Style,
+        size: Size
+    ) {
+        self.style = style
+        self.size = size
+    }
     
     public func update(
         with viewProperties: inout SectionMessageView.ViewProperties
     ) {
+        viewProperties.icon = .init(
+            image: style.icon(size: size),
+            size: size.iconSize())
+        viewProperties.backgroundColor = style.backgroundColor()
+        let fontStyles = size.fontStyles()
+        viewProperties.title = viewProperties.title?
+            .fontStyle(fontStyles.title)
+            .foregroundColor(style.textColor())
+        viewProperties.subtitle = viewProperties.subtitle?
+            .fontStyle(fontStyles.content)
+            .foregroundColor(style.textColor())
+        if let actionButton = viewProperties.bottomButton {
+            viewProperties.bottomButton?.text = actionButton.text?
+                .fontStyle(fontStyles.action)
+                .foregroundColor(style.actionColor())
+        }
+        viewProperties.cornerRadius = 8
+        viewProperties.insets = .init(top: 14, left: 16, bottom: 14, right: 16)
+    }
+}
+
+public extension SectionMessageStyle.Style {
+    
+    func icon(size: SectionMessageStyle.Size) -> UIImage {
+        
+        func smallIcon() -> UIImage {
+            switch self {
+            case .info: .ic16InfoCircleOutline
+            case .warning: .ic16WarningTriangleOutline
+            case .success: .ic16CheckCircleOutline
+            case .error: .ic16InfoCircleOutline
+            case .neutral: .ic16InfoCircleOutline
+            }
+        }
+        
+        func mediumIcon() -> UIImage {
+            switch self {
+            case .info: .ic24InfoCircleOutline
+            case .warning: .ic24WarningTriangleOutline
+            case .success: .ic24CheckCircleOutline
+            case .error: .ic24InfoCircleOutline
+            case .neutral: .ic24InfoCircleOutline
+            }
+        }
+        
+        func iconTintColor() -> UIColor {
+            switch self {
+            case .info: .contentInfo
+            case .warning: .contentWarning
+            case .success: .contentSuccess
+            case .error: .contentError
+            case .neutral: .contentPrimary
+            }
+        }
+        
+        switch size {
+        case .sizeS: return smallIcon().tinted(with: iconTintColor())
+        case .sizeM: return mediumIcon().tinted(with: iconTintColor())
+        }
+    }
+    
+    func textColor() -> UIColor { .contentPrimary }
+    
+    func backgroundColor() -> UIColor {
         switch self {
-        case .info:
-            viewProperties.title = viewProperties.title?.string.textM_1(color: .contentPrimary)
-            viewProperties.content = viewProperties.content?.string.textM_1(color: .contentPrimary)
-            viewProperties.subtitle = viewProperties.subtitle?.string.textM_1(color: .contentInfo)
-            viewProperties.iconImage = .ic24InfoCircleOutline.tinted(with: .contentInfo)
-            viewProperties.backgroundColor = .backgroundInfoLight
-        case .warning:
-            viewProperties.title =  viewProperties.title?.string.textM_1(color: .contentPrimary)
-            viewProperties.content = viewProperties.content?.string.textM_1(color: .contentPrimary)
-            viewProperties.subtitle = viewProperties.subtitle?.string.textM_1(color: .contentWarning)
-            viewProperties.iconImage = .ic24WarningTriangleOutline.tinted(with: .contentWarning)
-            viewProperties.backgroundColor = .backgroundWarningLight
-        case .success:
-            viewProperties.title = viewProperties.title?.string.textM_1(color: .contentPrimary)
-            viewProperties.content = viewProperties.content?.string.textM_1(color: .contentPrimary)
-            viewProperties.subtitle = viewProperties.subtitle?.string.textM_1(color: .contentSuccess)
-            viewProperties.iconImage = .ic24CheckCircleOutline.tinted(with: .contentSuccess)
-            viewProperties.backgroundColor = .backgroundSuccessLight
-        case .error:
-            viewProperties.title = viewProperties.title?.string.textM_1(color: .contentPrimary)
-            viewProperties.content = viewProperties.content?.string.textM_1(color: .contentPrimary)
-            viewProperties.subtitle = viewProperties.subtitle?.string.textM_1(color: .contentError)
-            viewProperties.iconImage = .ic24InfoCircleOutline.tinted(with: .contentInfo)
-            viewProperties.backgroundColor = .backgroundErrorLight
-        case .security:
-            viewProperties.title = viewProperties.title?.string.textM_1(color: .contentPrimary)
-            viewProperties.content = viewProperties.content?.string.textM_1(color: .contentPrimary)
-            viewProperties.subtitle = viewProperties.subtitle?.string.textM_1(color: .contentAction)
-            viewProperties.iconImage = .ic24Security.tinted(with: .contentInfo)
-            viewProperties.backgroundColor = .backgroundSecondary
+        case .info: .backgroundInfoLight
+        case .warning: .backgroundWarningLight
+        case .success: .backgroundSuccessLight
+        case .error: .backgroundErrorLight
+        case .neutral: .backgroundSecondary
+        }
+    }
+    
+    func actionColor() -> UIColor {
+        switch self {
+        case .info: .contentInfo
+        case .warning: .contentWarning
+        case .success: .contentSuccess
+        case .error: .contentError
+        case .neutral: .contentSuccess
+        }
+    }
+}
+
+public extension SectionMessageStyle.Size {
+    
+    func fontStyles() -> (title: FontStyle, content: FontStyle, action: FontStyle) {
+        switch self {
+        case .sizeS: (title: .textS_1, content: .textS, action: .textS)
+        case .sizeM: (title: .textM_1, content: .textM, action: .textM)
+        }
+    }
+    
+    func iconSize() -> CGSize {
+        switch self {
+        case .sizeS: .init(width: 16, height: 20)
+        case .sizeM: .init(width: 24, height: 24)
         }
     }
 }
