5e32fe1e5f82e613270b9ee5bef9a8a0d0c5b623
Сергеев Эдуард <sergeeveo@akbarsdigital.ru>
2024-04-12 18:41:23 +0700

    PCABO3-10262 ~ стандартизация стилей Tile

--- Sources/DesignSystem/Components/Examples/TileViewExample.swift
+++ Sources/DesignSystem/Components/Examples/TileViewExample.swift
@@ -1,13 +1,74 @@
 import UIKit
+import SnapKit
 import Components
+import ImagesService
+import Colors
 
-private func example() {
-    let view = TileView()
-    var viewProperties = TileView.ViewProperties(
-        icon: .init(variant: .icon(UIImage())),
-        text: .init(string: "Example"),
-        action: { print("Example") })
-    viewProperties = TileViewStyle.resize(size: .large, viewProperties: viewProperties)
-    viewProperties = TileViewStyle.restyle(style: .primary, viewProperties: viewProperties)
-    view.update(with: viewProperties)
+private class TileVC: UIViewController {
+    
+    private let tileView = TileView()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .black
+        
+        let vStack = vStack()
+        
+        vStack.addArrangedSubview({
+            let view = TileView()
+            let style = TileViewStyle(
+                size: .sizeL,
+                style: .action)
+            var viewProperties = TileView.ViewProperties(
+                icon: style.centeredIcon(.ic24Book.tinted(with: .contentPrimaryInverse)),
+                text: .init(string: "Example"),
+                action: { print("Example") })
+            style.update(viewProperties: &viewProperties)
+            view.update(with: viewProperties)
+            return view
+        }())
+        
+        vStack.addArrangedSubview({
+            let view = TileView()
+            let style = TileViewStyle(
+                size: .sizeL,
+                style: .main)
+            var viewProperties = TileView.ViewProperties(
+                icon: style.styledCenteredIcon(.ic24Car),
+                text: .init(string: "Example"),
+                action: { print("Example") })
+            style.update(viewProperties: &viewProperties)
+            view.update(with: viewProperties)
+            return view
+        }())
+        
+        vStack.addArrangedSubview({
+            let view = TileView()
+            let style = TileViewStyle(
+                size: .sizeL,
+                style: .primary)
+            var viewProperties = TileView.ViewProperties(
+                icon: style.styledCenteredIcon(.ic24Call),
+                text: .init(string: "Example"),
+                action: { print("Example") })
+            style.update(viewProperties: &viewProperties)
+            view.update(with: viewProperties)
+            return view
+        }())
+        
+        view.addSubview(vStack)
+        vStack.snp.makeConstraints {
+            $0.centerY.equalToSuperview()
+            $0.leading.trailing.equalToSuperview().inset(16)
+        }
+    }
+    
+    private func vStack() -> UIStackView {
+        let stack = UIStackView()
+        stack.axis = .vertical
+        stack.spacing = 16
+        stack.alignment = .center
+        return stack
+    }
 }
