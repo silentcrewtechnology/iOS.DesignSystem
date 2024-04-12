e4e1117e7a5d1cef4c6d73433b1e5b145a9ef791
Artur Gaisin <GaisinAE@akbarsdigital.ru>
2024-04-12 14:33:05 +0300

    PCABO3-10255 paper view style update

--- Sources/DesignSystem/Components/Styles/PaperViewStyle.swift
+++ Sources/DesignSystem/Components/Styles/PaperViewStyle.swift
@@ -8,7 +8,7 @@
 import UIKit
 import Components
 
-public enum PaperViewStyle {
+public struct PaperViewStyle {
     
     public enum CornerMask {
         case none
@@ -31,29 +31,39 @@ public enum PaperViewStyle {
         case custom(UIColor)
     }
     
-    public static func update(
+    private var mask: CornerMask
+    private var radius: CornerRadius
+    private var color: BackgroundColor
+    private var hasShadow: Bool
+    
+    public init(
         mask: CornerMask,
         radius: CornerRadius,
         color: BackgroundColor,
-        hasShadow: Bool = false,
-        viewProperties: PaperView.ViewProperties
-    ) -> PaperView.ViewProperties {
-        var viewProperties = viewProperties
-        viewProperties = updateCornerMask(mask: mask, viewProperties: viewProperties)
-        viewProperties = updateCornerRadius(radius: radius, viewProperties: viewProperties)
-        viewProperties = updateBackgroundColor(color: color, viewProperties: viewProperties)
-        viewProperties = updateShadow(hasShadow: hasShadow, viewProperties: viewProperties)
-        return viewProperties
+        hasShadow: Bool
+    ) {
+        self.mask = mask
+        self.radius = radius
+        self.color = color
+        self.hasShadow = hasShadow
     }
     
-    public static func updateCornerMask(
+    public func update(
+        viewProperties: inout PaperView.ViewProperties
+    ) {
+        updateCornerMask(mask: mask, viewProperties: &viewProperties)
+        updateCornerRadius(radius: radius, viewProperties: &viewProperties)
+        updateBackgroundColor(color: color, viewProperties: &viewProperties)
+        updateShadow(hasShadow: hasShadow, viewProperties: &viewProperties)
+    }
+    
+    private func updateCornerMask(
         mask: CornerMask,
-        viewProperties: PaperView.ViewProperties
-    ) -> PaperView.ViewProperties {
-        var viewProperties = viewProperties
+        viewProperties: inout PaperView.ViewProperties
+    ) {
         switch mask {
         case .none:
-            viewProperties = updateCornerRadius(radius: .none, viewProperties: viewProperties)
+            updateCornerRadius(radius: .none, viewProperties: &viewProperties)
         case .top:
             viewProperties.maskedCorners = [.layerMinXMinYCorner, .layerMaxXMinYCorner]
         case .bottom:
@@ -61,16 +71,15 @@ public enum PaperViewStyle {
         case .all:
             viewProperties.maskedCorners = [.layerMinXMinYCorner, .layerMaxXMinYCorner, .layerMinXMaxYCorner, .layerMaxXMaxYCorner]
         case let .automatic(rowsInSection, indexPath):
-            viewProperties = updateAutomaticCornerMask(rows: rowsInSection, indexPath: indexPath, viewProperties: viewProperties)
+            updateAutomaticCornerMask(rows: rowsInSection, indexPath: indexPath, viewProperties: &viewProperties)
         }
-        return viewProperties
     }
     
-    private static func updateAutomaticCornerMask(
+    private func updateAutomaticCornerMask(
         rows: Int,
         indexPath: IndexPath,
-        viewProperties: PaperView.ViewProperties
-    ) -> PaperView.ViewProperties {
+        viewProperties: inout PaperView.ViewProperties
+    ) {
         let automaticStyle: CornerMask
         switch (rows, indexPath.row) {
         case (1, _):
@@ -82,14 +91,13 @@ public enum PaperViewStyle {
         default:
             automaticStyle = .none
         }
-        return updateCornerMask(mask: automaticStyle, viewProperties: viewProperties)
+        updateCornerMask(mask: automaticStyle, viewProperties: &viewProperties)
     }
     
-    public static func updateCornerRadius(
+    private func updateCornerRadius(
         radius: CornerRadius,
-        viewProperties: PaperView.ViewProperties
-    ) -> PaperView.ViewProperties {
-        var viewProperties = viewProperties
+        viewProperties: inout PaperView.ViewProperties
+    ) {
         switch radius {
         case .none:
             viewProperties.cornerRadius = 0
@@ -100,14 +108,12 @@ public enum PaperViewStyle {
         case .custom(let cornerRadius):
             viewProperties.cornerRadius = cornerRadius
         }
-        return viewProperties
     }
     
-    public static func updateBackgroundColor(
+    private func updateBackgroundColor(
         color: BackgroundColor,
-        viewProperties: PaperView.ViewProperties
-    ) -> PaperView.ViewProperties {
-        var viewProperties = viewProperties
+        viewProperties: inout PaperView.ViewProperties
+    ) {
         switch color {
         case .main:
             viewProperties.backgroundColor = .backgroundMain
@@ -116,14 +122,12 @@ public enum PaperViewStyle {
         case .custom(let backgroundColor):
             viewProperties.backgroundColor = backgroundColor
         }
-        return viewProperties
     }
     
-    public static func updateShadow(
+    private func updateShadow(
         hasShadow: Bool,
-        viewProperties: PaperView.ViewProperties
-    ) -> PaperView.ViewProperties {
-        var viewProperties = viewProperties
+        viewProperties: inout PaperView.ViewProperties
+    ) {
         if hasShadow {
             viewProperties.masksToBounds = false
             viewProperties.shadow = .init(
@@ -139,6 +143,5 @@ public enum PaperViewStyle {
                 opacity: 0,
                 radius: 0)
         }
-        return viewProperties
     }
 }
