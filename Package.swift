1391cc925f3714f41f8dbee1ad9ae6eed9a4bf60
Ilnur Mugaev <ilnur.mugaev@akbarsdigital.ru>
2024-05-20 14:09:33 +0300

    PCABO3-10673: Обновил пакеты до следующих минорных версий.

--- Package.swift
+++ Package.swift
@@ -16,10 +16,10 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(url: "https://gitlab.akbars.tech/abo/ios-resources-fonts.git", exact: "0.0.1"),
-        .package(url: "https://gitlab.akbars.tech/abo/ios-resources-colors.git", exact: "0.0.3"),
-        .package(url: "https://gitlab.akbars.tech/abo/ios-resources-icons.git", exact: "0.0.2"),
-        .package(url: "https://gitlab.akbars.tech/abo/ios-resources-ui-components.git", exact: "0.0.46")
+        .package(url: "https://gitlab.akbars.tech/abo/ios-resources-fonts.git", .upToNextMinor(from: "0.0.1")),
+        .package(url: "https://gitlab.akbars.tech/abo/ios-resources-colors.git", .upToNextMinor(from: "0.0.3")),
+        .package(url: "https://gitlab.akbars.tech/abo/ios-resources-icons.git", .upToNextMinor(from: "0.0.2")),
+        .package(url: "https://gitlab.akbars.tech/abo/ios-resources-ui-components.git", .upToNextMinor(from: "0.0.46")),
     ],
     targets: [
         .target(
