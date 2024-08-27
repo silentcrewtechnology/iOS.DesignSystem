990f8e982b9964f71bdf16b00876ecdf1b9e3e7e
KaluginAA <KaluginAA@akbarsdigital.ru>
2024-08-27 15:41:49 +0300

    PCABO3-10760: Добавил новые поля в DSRowModel

--- Sources/DesignSystem/Components/Styles/Rows/Services/DSRowModel.swift
+++ Sources/DesignSystem/Components/Styles/Rows/Services/DSRowModel.swift
@@ -6,22 +6,33 @@ final public class DSRowModel {
     public let center: DSRowBlocks?
     public let trailing: DSRowBlocks?
     public let centralBlockAlignment: RowBaseContainer.ViewProperties.BlockAlignment
+    public let margings: RowBaseContainer.ViewProperties.Margins?
     public let cellIdentifier: String
     public let cellSelectionStyle: UITableViewCell.SelectionStyle
+    /// Для разной высоты ячеек в одной секции.
+    /// Если нужна одинаковая высота у всех ячеек в секции - использовать rowsHeight у SectionRowModelService
+    public let height: CGFloat?
+    public let didTap: ((UITableView, IndexPath) -> Void)?
     
     public init(
         leading: DSRowBlocks? = nil,
         center: DSRowBlocks? = nil,
         trailing: DSRowBlocks? = nil,
         centralBlockAlignment: RowBaseContainer.ViewProperties.BlockAlignment = .leading,
+        margings: RowBaseContainer.ViewProperties.Margins? = nil,
         cellIdentifier: String = "RowCell",
-        cellSelectionStyle: UITableViewCell.SelectionStyle = .gray
+        cellSelectionStyle: UITableViewCell.SelectionStyle = .gray,
+        height: CGFloat? = nil,
+        didTap: ((UITableView, IndexPath) -> Void)? = nil
     ) {
         self.leading = leading
         self.center = center
         self.trailing = trailing
         self.centralBlockAlignment = centralBlockAlignment
+        self.margings = margings
         self.cellIdentifier = cellIdentifier
         self.cellSelectionStyle = cellSelectionStyle
+        self.height = height
+        self.didTap = didTap
     }
 }
