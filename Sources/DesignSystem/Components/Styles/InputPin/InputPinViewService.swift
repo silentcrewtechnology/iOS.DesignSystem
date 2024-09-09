470962964c7d83df22e7ffc25856578263a8ba56
Сергеев Эдуард Олегович <sergeeveo@akbarsdigital.ru>
2024-09-09 14:29:00 +0700

    + стиль InputPin

--- Sources/DesignSystem/Components/Styles/InputPin/InputPinViewService.swift
+++ Sources/DesignSystem/Components/Styles/InputPin/InputPinViewService.swift
@@ -7,15 +7,18 @@ public final class InputPinViewService {
     
     public private(set) var view: V
     public private(set) var viewProperties: V.ViewProperties
+    public private(set) var style: InputPinViewStyle
     public private(set) var itemServices: [InputPinItemViewService]
     
     public init(
         view: V = .init(),
         viewProperties: V.ViewProperties = .init(),
+        style: InputPinViewStyle = .init(),
         itemServices: [InputPinItemViewService] = []
     ) {
         self.view = view
         self.viewProperties = viewProperties
+        self.style = style
         self.itemServices = itemServices
         setupItems()
         update()
@@ -26,6 +29,7 @@ public final class InputPinViewService {
     }
     
     private func update() {
+        style.update(viewProperties: &viewProperties)
         view.update(with: viewProperties)
     }
     
