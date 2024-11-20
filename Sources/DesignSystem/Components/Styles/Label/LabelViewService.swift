71e5988926c9f32fb4c27730057ff2c2fac67bf6
KaluginAA <KaluginAA@akbarsdigital.ru>
2024-11-20 10:21:06 +0300

    PCABO3-11355: Аткуализация cтиля Label

--- Sources/DesignSystem/Components/Styles/Label/LabelViewService.swift
+++ Sources/DesignSystem/Components/Styles/Label/LabelViewService.swift
@@ -2,9 +2,14 @@ import UIKit
 import Components
 
 public final class LabelViewService {
+    
+    // MARK: - Properties
+    
     public private(set) var view: LabelView
     public private(set) var viewProperties: LabelView.ViewProperties
     public private(set) var style: LabelViewStyle
+    
+    // MARK: - Init
     
     public init(
         view: LabelView = .init(),
@@ -18,9 +23,16 @@ public final class LabelViewService {
         update()
     }
     
+    // MARK: - Methods
+    
     public func update(
-        newVariant: LabelViewStyle.Variant? = nil
+        newVariant: LabelViewStyle.Variant? = nil,
+        newText: NSMutableAttributedString? = nil
     ) {
+        if let newText {
+            viewProperties.text = newText
+        }
+        
         style.update(
             variant: newVariant,
             viewProperties: &viewProperties
